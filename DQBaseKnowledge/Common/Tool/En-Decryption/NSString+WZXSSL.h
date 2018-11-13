273bd051905213dfda754bbfdc5f6e80f75abaa0
琪琪邓 <15013781186@163.com>
2018-11-13 21:38:21 +0800

    webSocket --

---
+++ DQBaseKnowledge/Common/Tool/En-Decryption/NSString+WZXSSL.h
@@ -0,0 +1,32 @@
+//
+//  NSString+WZXSSLTool.h
+//  WZXSSLTool
+//
+//  Created by wordoor－z on 16/3/28.
+//  Copyright © 2016年 wzx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (WZXSSL)
+
+- (NSString *)do16MD5;
+
+- (NSString *)do32MD5;
+
+- (NSString *)doSha1;
+
+- (NSString *)doBase64;
+
+- (NSString *)decodeBase64;
+
+- (NSString *)doAES:(NSString *)key;
+
+- (NSString *)decodeAES:(NSString *)key;
+
+- (NSString *)doDES:(NSString *)key;
+
+- (NSString *)decodeDES:(NSString *)key;
+
+
+@end
